8d06f2e48779fc6869559fe6935e02e6046f8b2d
AnotherPlanet <38158221+AsniLife@users.noreply.github.com>
2018-05-28 21:11:02 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,102 @@
+-- Created by Marcel Clément
+-- Si vous avez à le modifier demandez le moi pour vous l'autorisez
+
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+ESX = nil
+local holstered = true
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	PlayerData = xPlayer
+end)
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+local skins = {
+	"mp_m_freemode_01",
+}
+
+-- L'animation de radio
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait( 0 )
+		local ped = PlayerPedId()
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and CheckSkin(ped) then
+			if not IsPauseMenuActive() then 
+				loadAnimDict( "random@arrests" )
+				if IsControlJustReleased( 0, 20 ) then -- La touche est (Z)
+					TriggerServerEvent('InteractSound_SV:PlayOnSource', 'off', 0.1)
+					TriggerEvent("esx_radio:notify", "CHAR_CALL911", 1, "911", false, 'Vous ne parlez plus à la radio')
+					ClearPedTasks(ped)
+				else
+					if IsControlJustPressed( 0, 20 ) and CheckSkin(ped) and not IsPlayerFreeAiming(PlayerId()) then -- La touche est (Z)
+						TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
+						TriggerEvent("esx_radio:notify", "CHAR_CALL911", 1, "911", false, 'Vous parlez désormais à la radio')
+						TaskPlayAnim(ped, "random@arrests", "generic_radio_chatter", 8.0, 2.5, -1, 49, 0, 0, 0, 0 )
+						SetCurrentPedWeapon(ped, GetHashKey("GENERIC_RADIO_CHATTER"), true)
+					elseif IsControlJustPressed( 0, 20 ) and CheckSkin(ped) and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+						TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
+						TriggerEvent("esx_radio:notify", "CHAR_CALL911", 1, "911", false, 'Vous parlez désormais à la radio')
+						TaskPlayAnim(ped, "random@arrests", "generic_radio_chatter", 8.0, 2.5, -1, 49, 0, 0, 0, 0 )
+						SetCurrentPedWeapon(ped, GetHashKey("GENERIC_RADIO_CHATTER"), true)
+					end 
+					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_chatter", 3) then
+						DisableActions(ped)
+					elseif IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_chatter", 3) then
+						DisableActions(ped)
+					end
+				end
+			end 
+		end 
+	end
+end )
+
+function CheckSkin(ped)
+	for i = 1, #skins do
+		if GetHashKey(skins[i]) == GetEntityModel(ped) then
+			return true
+		end
+	end
+	return false
+end
+
+function DisableActions(ped)
+	DisableControlAction(1, 140, true)
+	DisableControlAction(1, 141, true)
+	DisableControlAction(1, 142, true)
+	DisableControlAction(1, 37, true)
+	DisablePlayerFiring(ped, true)
+end
+
+function loadAnimDict( dict )
+	while ( not HasAnimDictLoaded( dict ) ) do
+		RequestAnimDict( dict )
+		Citizen.Wait( 0 )
+	end
+end
+
+RegisterNetEvent("esx_radio:notify")
+AddEventHandler("esx_radio:notify", function(icon, type, sender, title, text)
+    Citizen.CreateThread(function()
+		Wait(1)
+		SetNotificationTextEntry("STRING");
+		AddTextComponentString(text);
+		SetNotificationMessage(icon, icon, true, type, sender, title, text);
+		DrawNotification(false, true);
+    end)
+end)
